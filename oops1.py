51fc46782d1bd24b0dc7b1b963efc75a3bdeb49a
shubham <2022ca61f@sigce.edu.in>
2025-03-16 21:49:39 +0530

    class and obj basics done

---
+++ oops1.py
@@ -0,0 +1,23 @@
+#initialize a class
+class employee:
+    #special method - constructer
+    def __init__(self):
+        print("Started executing attributes/data")
+        self.id = 123
+        self.salary = 50000
+        self.designation = "SDH"
+        print("attributes/data have been initiated")
+
+    def travel(self, destination):
+        print("this travel method was called manually")
+        print(f"Employee is now traveling to {destination}")
+
+#creat a obj/instance of the class
+sam = employee()
+
+#print(sam.id)
+#print(sam.salary)
+#print(sam.designation)
+#sam.travel("kerala")
+
+print(type(sam))
